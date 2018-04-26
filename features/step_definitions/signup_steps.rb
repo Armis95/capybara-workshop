ac1d2fb4c1e2e5cec6430f4995e9d55d2332c4ba
Armis <armands.locmelis@testdevlab.com>
2018-04-26 13:57:08 +0300

    HW3 without 1.9 & 1.10 points

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,4 +1,28 @@
 When(/^I am on Appimation home page/) do
   visit('/')
   find(:css, '#start_button')
+  find(:xpath, "//button[@id = 'start_button']")
+
+  find(:css, '#signup-b')
+  find(:xpath, "//button[@id = 'signup-b']")
+
+  find(:css, '#cta')
+  find(:xpath, "//section[@id = 'cta']")
+
+  find(:css, '#name')
+  find(:xpath, "//*[@id='name']")
+
+  
+  find(:css, '#email')
+  find(:xpath, "//*[@id='email']")
+  
+  find(:css, '#contactus-message')
+  find(:xpath, "//*[@id='contactus-message']")
+
+  find(:css, '#contactus-button')
+  find(:xpath, "//*[@id='contactus-button']")
+
+
+  find(:css, '.fa-facebook')
+  find(:xpath, "//*[@id='footer']//*[@class = 'icon fa-facebook']")
 end
