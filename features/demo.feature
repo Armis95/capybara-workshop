2ec9f58b03fc6fde1e25cfe9bf00f8d92544ff43
Armis <armands.locmelis@testdevlab.com>
2018-04-26 17:53:01 +0300

    LEcture3 darbs

--- features/demo.feature
+++ features/demo.feature
@@ -2,3 +2,5 @@ Feature: Demo Feature
 
     Scenario: Open SignUp Page
         Given I am on Appimation home page
+        When I open Try now
+        Then I close Try now
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,8 +1,17 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-  find(:css, '#start_button')
-  find(:xpath, "//button[@id = 'start_button']")
-  find(:css, "#video")
-  find(:css, ".feature-image")
-  find(:xpath, "//h3[contains(text(), 'Team up')]/preceding-sibling::img")
+  unless find(:css, '#logo').visible?
+    raise "Logo not visible"
+  end
+end
+
+When(/^I open Try now/) do
+  find(:css, '#start_button').visible?
+  unless find(:css, '#signup')
+    raise "Not found"
+  end
+end
+
+Then(/^I close Try now/) do 
+  find(:css, '#signup .closecross').click
 end
