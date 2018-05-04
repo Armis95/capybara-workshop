914f8839a8a15a919b502c882ba84a9627a4679d
Armis <armands.locmelis@testdevlab.com>
2018-05-04 23:50:06 +0300

    HW4 firstUpdate

--- features/demo.feature
+++ features/demo.feature
@@ -3,8 +3,11 @@ Feature: Demo Feature
     Scenario: Open SignUp Page
         Given I am on Appimation home page
         When I open Try now
+        Then I enter all required information "armis@grr.la" "name123" "name123" "project"
         Then I close Try now
 
-    Scenario: I can send contact us message
+    Scenario: Open SignIn Page
         Given I am on Appimation home page
-        When I enter "lalalalla" and "namemname" in name in contact us
+        When I open SignIn page
+        Then I enter all required information "armis@grr.la" "parole123"
+        Then I try to sign in
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,11 +1,9 @@
 When(/^I am on Appimation home page/) do
-  visit('/')
+  visit('legacy')
   unless find(:css, '#logo').visible?
     raise "Logo not visible"
   end
 end
-
-
 
 When(/^I open Try now/) do
   find(:css, '#start_button').click
@@ -14,23 +12,36 @@ When(/^I open Try now/) do
   end
 end
 
+Then("I enter all required information {string} {string} {string} {string}") do |email, password1, password2, projectName|
+  find(:css, '#signup input[name="login"]').send_keys(email)
+  find(:css, '#signup input[name="password1"]').send_keys(password1)
+  find(:css, '#signup input[name="password2"]').send_keys(password2)
+  find(:css, '#signup input[name="project_name"]').send_keys(projectName)
+end 
 
 Then("I close Try now") do
   find(:css, '#signup .closecross').click
-end
-
-
-When("I enter {string} and {string} in name in contact us") do |name, name2|
-  unless find(:css, '#name').visible?
-    raise "name not visible"
+  unless find(:css, '#logo').visible?
+    raise "Logo not visible"
   end
-  find(:css, '#name').send_keys(name)
-  sleep(2)
-  find(:css, '#name').native.clear
-
-  find(:css, '#name').send_keys(name2)
-  sleep(2)
 end
+
+When("I open SignIn page") do
+  find(:css, '#login-b').click
+  unless find(:css, '#login')
+    raise "Sign in page not visible"
+  end
+end
+
+Then("I enter all required information {string} {string}") do |email, password|
+  find(:css, '#login input[name="login"]').send_keys(email)
+  find(:css, '#login input[name="password"]').send_keys(password)  
+end
+
+Then("I try to sign in") do
+  find(:css, '.button.button.button-block.innerButton').click
+end
+
 
 
 
